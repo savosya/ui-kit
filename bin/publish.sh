f17360d5fb5c85b50603bd1e68fe0d5f32b9af30
Dmitriy Savostyanov <svstnv.dm@gmail.com>
2023-06-22 01:39:52 +0700

    fix

--- bin/publish.sh
+++ bin/publish.sh
@@ -10,8 +10,8 @@ yarn build
 # публикую все подпакеты
 lerna publish from-git
 # публикую корневой проект
-yarn publish build --no-git-tag-version
+npm publish build --no-git-tag-version
 # обновляю версию в корневом пакете, генерирую CHANGELOG.MD, делаю коммит, создаю git-tag
-yarn release --release-as $RELEASE_TYPE
+npm release --release-as $RELEASE_TYPE
 # отправляю изменения на github
 git push --follow-tags
