40bc79e920859f50a5c83436ffdbd2786c085892
Dmitriy Savostyanov <svstnv.dm@gmail.com>
2023-07-11 02:03:04 +0700

    imports fix

--- bin/create-root-exporter-file.sh
+++ bin/create-root-exporter-file.sh
@@ -10,9 +10,11 @@ PACKAGES=$(ls packages)
 
 for pkg in $PACKAGES; do
   CLEAN_PKG=$(echo $pkg | sed 's/@.*\///' | tr '/' '-')
+  CAMEL_CASE_PKG=$(echo $CLEAN_PKG | tr '-' ' ' | awk '{ for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2); print }' | tr -d ' ')
 
   if [ "$CLEAN_PKG" != "variables" ]; then
-    echo "export * from './$CLEAN_PKG';" >> $OUTPUT_FILE
+#    echo "export * from './$CLEAN_PKG';" >> $OUTPUT_FILE
+    echo "export { default as ${CAMEL_CASE_PKG} } from './${CLEAN_PKG}';" >> $OUTPUT_FILE
   fi
 done
 
