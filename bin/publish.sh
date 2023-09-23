9f0a569e0a3e12e06cf1c7da666446136f09445f
Dmitriy Savostyanov <svstnv.dm@gmail.com>
2023-09-23 17:09:46 +0700

    Publish root package: v0.1.2

--- bin/publish.sh
+++ bin/publish.sh
@@ -32,8 +32,7 @@ lerna publish from-package --no-push --no-commit-hooks
 
 # обновляю версию в корневом пакете, генерирую CHANGELOG.MD, делаю коммит, создаю git-tag
 # npm release --release-as $RELEASE_TYPE
-git tag -a "v${NEW_ROOT_PACKAGE_VERSION}" -m "v${NEW_ROOT_PACKAGE_VERSION}" && git push --tags
-# git push origin main
+git tag -a "v${NEW_ROOT_PACKAGE_VERSION}" -m "v${NEW_ROOT_PACKAGE_VERSION}" && git push --tags && git push origin main
 
 
 
