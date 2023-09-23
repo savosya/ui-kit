518fa4af8d7e2be7c52b173a2f70f1807e7b2ae0
Dmitriy Savostyanov <svstnv.dm@gmail.com>
2023-09-23 18:02:51 +0700

    add force pub to publish script

--- bin/publish.sh
+++ bin/publish.sh
@@ -6,6 +6,25 @@
 # выхожу, если одна из команд завершилась неудачно
 set -e
 
+# Читаем флаги (-f = forcePublishAll)
+force=false
+
+# Process command-line options
+while getopts ":f" opt; do
+  case ${opt} in
+    f )
+      force=true
+      ;;
+    \? )
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+
+
 # беру текущую git ветку и кладу ее значение в current_branch
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
@@ -15,24 +34,31 @@ if [ "$current_branch" != "main" ]; then
     exit 1
 fi
 
-# поднимаю версию во всех подпакетах (если есть изменения)
-lerna version --no-push --no-commit-hooks --no-git-tag-version
-
-# commit new tag (получаю новую версию рут пакета)
-NEW_ROOT_PACKAGE_VERSION=$(node -p "require('./packages/root/package.json').version")
-
-# Commit new tag. Создаю тег для гита (тег = версия рут пакета)
-git commit -a -m "Publish root package: v${NEW_ROOT_PACKAGE_VERSION}"
 
 # сборка всех пакетов
 yarn build
 
+# поднимаю версию во всех подпакетах (если есть изменения)
+if [ "$force" = true ]; then
+  lerna version --force-publish --no-push --no-commit-hooks --no-git-tag-version
+else
+  lerna version --no-push --no-commit-hooks --no-git-tag-version
+fi
+
+# commit new tag (получаю новую версию рут пакета)
+NEW_ROOT_PACKAGE_VERSION=$(node -p "require('./packages/root/package.json').version")
+
+#сборка всех пакетов
+yarn build
+
+# Commit new tag. Создаю тег для гита (тег = версия рут пакета)
+git commit -a -m "Publish root package: v${NEW_ROOT_PACKAGE_VERSION}"
+
 # публикую все пакеты, включая рут (если есть изменения)
 lerna publish from-package --no-push --no-commit-hooks
+
 
 # обновляю версию в корневом пакете, генерирую CHANGELOG.MD, делаю коммит, создаю git-tag
 # npm release --release-as $RELEASE_TYPE
 git tag -a "v${NEW_ROOT_PACKAGE_VERSION}" -m "v${NEW_ROOT_PACKAGE_VERSION}" && git push --tags && git push origin main
-
-
 
